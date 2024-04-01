c49f07481be19cc133a060d4dd3a0ffbd16eb930
Nikita <Nikita@NICKY-PC>
2024-04-01 17:08:20 +0300

    Добавьте файлы проекта.

---
+++ Calc/Calc.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+float adding(float firstNUM, float secondNUM)
+{
+    return firstNUM + secondNUM;
+}
+
+float substracting(float firstNUM, float secondNUM)
+{
+    return firstNUM - secondNUM;
+}
+
+float division(float firstNUM, float secondNUM)
+{
+    return firstNUM / secondNUM;
+}
+
+float multipling(float firstNUM, float secondNUM)
+{
+    return firstNUM * secondNUM;
+}
+
+int main()
+{
+    float a, b;
+    char oper;
+    std::cout << "Type two numbers for operation: ";
+    std::cin >> a >> b;
+    std::cout << "Type sign of operation: ";
+    std::cin >> oper;
+    switch (oper)
+    {
+        case '+':
+            std::cout << "Result of adding: " << a << " + " << b << " = " << adding(a, b) << std::endl;
+            break;
+        case '-':
+            std::cout << "Result of substracting: " << a << " - " << b << " = " << substracting(a, b) << std::endl;
+            break;
+        case '/':
+            std::cout << "Result of division: " << a << " / " << b << " = " << division(a, b) << std::endl;
+            break;
+        case '*':
+            std::cout << "Result of multipling: " << a << " * " << b << " = " << multipling(a, b) << std::endl;
+            break;
+    default:
+        std::cout << "Uncorrect sign of operation\n";
+        break;
+    }
+    return 0;
+}
